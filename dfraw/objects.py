10e82607a80adb434ba1c1df4099cd66296015de
lethosor <stahlalan19@gmail.com>
2013-05-05 13:41:47 -0400

    Add object support with getters

--- dfraw/objects.py
+++ dfraw/objects.py
@@ -26,7 +26,8 @@ class File:
 		self.raw_string = f.read()
 		f.close()
 		self.token_list = TokenList(self.raw_string)
-	
+		
+		self.object_list = ObjectList(self.token_list)
 
 class Token:
 	def __init__(self, string):
@@ -35,7 +36,7 @@ class Token:
 		else:
 			self.type = Token.COMMENT
 		self.value = string
-		if self.value.replace('\n','').replace('\t','').replace('\r','') == '':
+		if self.value.replace('\n','').replace('\t','').replace('\r','').replace(' ','') == '':
 			self.value = ''
 		if self.type == Token.TAG:
 			self.list = self.value[1:-1].split(':')
@@ -51,7 +52,11 @@ class Token:
 			# Comment; acts like a string
 			return self.value[index]
 		else:
-			return self.list[index]
+			#print self.type, index, self.list
+			try:
+				return self.list[index]
+			except IndexError:
+				return None
 		
 	
 # Constants
@@ -68,7 +73,6 @@ class TokenList:
 		i = j = 0
 		tag = False
 		while i-1 < len(string):
-			#print i, j, tag, arr
 			if tag:
 				# Inside a tag
 				try:
@@ -99,9 +103,148 @@ class TokenList:
 		for i, token in enumerate(self.list):
 			if token.type == Token.COMMENT and token.value == '':
 				del self.list[i]
-	
-	def __getitem__(self, index):
-		return self.list[index]	
 		
+		
+	def __getitem__(self, index):
+		return self.list[index]
+	
+	def __len__(self):
+		return len(self.list)	
+		
+class Object:
+	def __init__(self, tokens):
+		self.list = tokens
+		self.flags = ObjectGetterFlags(tokens)
+		self.tags = ObjectGetterTags(tokens)
+		self.all_tags = ObjectGetterAllTags(tokens)
+		self.name = self.tags.name
+	
+	def __getitem__(self, tag):
+		"""
+		Get a tag with type detection
+		Returns:
+		* False if the tag does not exist
+		* True if the tag exists with no arguments
+		* The tag's first argument, if the tag has only one argument
+		* All of the tag's arguments, if the tag has multiple arguments
+		* All of the tag's occurences' arguments, if more than 1 occurence
+		"""
+		result = self.all_tags[tag]
+		if self.tags[tag] == False:
+			# Tag does not occur at all
+			return False
+		elif result == [[]]:
+			# Tag occurs once with no arguments
+			return True
+		elif len(result[0]) == 1:
+			# Only 1 occurence, which has only 1 argument
+			return result[0][0]
+		elif len(result) == 1:
+			# Only 1 occurence of tag
+			return result[1]
+		# Something else
+		return result
+	__getattr__ = __getitem__
+	
+class ObjectGetterFlags:
+	def __init__(self, tokens):
+		self.tokens = tokens
+	def __getitem__(self, tag):
+		# Returns True if the given tag name exists, False otherwise
+		tag = tag.upper()
+		for t in self.tokens:
+			if t.type == Token.TAG and t[0] == tag:
+				return True
+		return False
+	# Use the same functionality for attributes
+	__getattr__ = __getitem__
+
+class ObjectGetterTags:
+	"""
+	An object that allows access to tags with arguments via bracket/attribute
+	syntax
+	Returns a list of arguments if found, an empty list if no arguments, or 
+	False if no matching tag
+	"""
+	def __init__(self, tokens):
+		self.tokens = tokens
+	def __getitem__(self, tag):
+		"""
+		Returns all arguments of the given tag
+		"""
+		tag = tag.upper()
+		for t in self.tokens:
+			if t.type == Token.TAG and t[0] == tag:
+				return t[1:]
+		return False
+	# Use the same functionality for attributes
+	__getattr__ = __getitem__
+	
+class ObjectGetterAllTags:
+	"""
+	Similar to ObjectGetterTags, but returns a list with all matching tags
+	"""
+	def __init__(self, tokens):
+		self.tokens = tokens
+	def __getitem__(self, tag):
+		"""
+		Returns a list of all matching tags
+		"""
+		list = []
+		tag = tag.upper()
+		for t in self.tokens:
+			if t.type == Token.TAG and t[0] == tag:
+				list.append(t[1:])
+		return list
+	# Use the same functionality for attributes
+	__getattr__ = __getitem__
+
+
+class ObjectList:
+	def __init__(self, token_list):
+		for i, token in enumerate(token_list):
+			if token[0] == 'OBJECT':
+				self.delimiter = token[1]
+				start = i + 1
+				break
+		
+		# A list of the starting locations of objects
+		self.starts = []
+		for i, token in enumerate(token_list):
+			if i< start:
+				continue
+			if token[0] == self.delimiter:
+				print token
+				# Include all comments before the first tag 
+				j = i-1
+				while token_list[j].type != Token.TAG:
+					j -= 1
+				j += 1
+				self.starts.append(j)
+		
+		t_list = []
+		for i, v in enumerate(self.starts):
+			t_list.append(token_list[v:self.starts[i+1] if i+1 < len(self.starts) else None])
+		self.t_list = t_list
+		
+		self.list = []
+		for tokens in self.t_list:
+			self.list.append(Object(tokens))
+		
+	def __getitem__(self, index):
+		"""
+		Returns:
+		* Object at the given index in self.list, if it exists
+		* Object with the given name in self.list, if exists
+		
+		Raises IndexError otherwise
+		"""	
+		if index < len(self.list) and index >= 0:
+			return self.list[int(index)]
+		for obj in self.list:
+			# Names are lowercase in DF
+			if index.lower() in obj.name:
+				return obj
+		raise IndexError('Object not found in object list: {0}'.format(index))
 		
 
