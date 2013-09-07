262e09e1110656d44b5f1f7869464f2d0717ab3c
lethosor <stahlalan19@gmail.com>
2013-09-07 17:06:01 -0400

    Documentation

--- dfraw/objects.py
+++ dfraw/objects.py
@@ -5,6 +5,11 @@ Objects
 __metaclass__ = type
 
 class Token:
+    """
+    A token (either a tag or comment).
+    
+    This class instantiates different token classes based on the value given.
+    """
     def __new__(cls, text, *args, **kwargs):
         if cls != Token:
             return object.__new__(cls)
@@ -14,19 +19,33 @@ class Token:
             return CommentToken(text, *args, **kwargs)
     
     def __repr__(self):
+        """
+        Returns a representation of this token (with legal Python syntax).
+        
+        >>> repr(TagToken('[foo]'))
+        TagToken('foo')
+        """
         return "{0}({1})".format(self.__class__.__name__, repr(str(self)))
     
     
-class TagToken(Token):    
+class TagToken(Token):
+    """
+    A token that represents a tag.
+    
+    Examples: "[a:b]", "[foo]", "[spam:eggs:spam:spam]"
+    """
     def __init__(self, text):
+        # Initializes self.name and self.args
         self.value = text[1:-1].split(':')
     
     def __unicode__(self):
+        """ The tag's original text """
         return '[' + ':'.join(self.value) + ']'
     __str__ = __unicode__
     
     @property
     def value(self):
+        """ A list containing the tag's name and arguments """
         return [self.name] + self.args
     
     @value.setter
@@ -35,10 +54,16 @@ class TagToken(Token):
 
     
 class CommentToken(Token):
+    """
+    A token that represents anything other than a tag (i.e. a comment).
+    
+    Example: "Example"
+    """
     def __init__(self, text):
         self.value = text
     
     def __unicode__(self):
+        """ The text of the comment """
         return self.value
     __str__ = __unicode__
 
